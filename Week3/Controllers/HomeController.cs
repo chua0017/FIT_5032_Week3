dc2e211c5bfee088d36d9285707beefd6041c6fe
chua0017 <chua0017@student.monash.edu>
2019-08-21 14:39:06 +1000

    add gitignore and gitattributes

---
+++ Week3/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Week3.HelloWorld;
+using Week3.Exercise;
+
+namespace Week3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            //ViewBag.Message = "Your application description page.";
+            Hello hello = new Hello();
+            ViewBag.message = hello.GetHello();
+            ExampleDictionary ed = new ExampleDictionary();
+            ed.Example();
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Week3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Week3.Startup))]
+namespace Week3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
